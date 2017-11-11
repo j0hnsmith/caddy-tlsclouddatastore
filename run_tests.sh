010c3e6060f0642fba71aef8fff7e86326e4b37c
j0hnsmith <info@whywouldwe.com>
2017-11-11 10:50:00 +0000

    run tests with -v

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
+
 # run the emulator
 gcloud beta emulators datastore start > /dev/null 2>&1 &
 emulator_pid=$!
 $(gcloud beta emulators datastore env-init)
+echo "If tests don't run, set DATASTORE_EMULATOR_HOST=localhost:{emulator_port}"
 
 
 # run the tests
-go test .
+go test . -v
 
 # killing $emulator_pid doesn't always propagate
 ps -ef | grep gcloud | grep emulator | awk '{ print $2 }' | xargs kill -2
